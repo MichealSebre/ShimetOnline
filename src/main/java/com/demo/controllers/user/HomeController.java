8be49d0bc97d14ec3bc02043ca6760b4207209a0
MichealSebre <msebre@gmail.com>
2020-04-02 17:31:23 -0400

    Initial commit

---
+++ src/main/java/com/demo/controllers/user/HomeController.java
@@ -0,0 +1,18 @@
+package com.demo.controllers.user;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import javax.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller 
+@RequestMapping("home")
+public class HomeController {
+
+	@RequestMapping (method =  RequestMethod.GET)
+	  public String index() {
+		
+		  return "home.index"; 
+	  }
+}
